441c2925a44ca91f7996357431f89eb3acd6b9b2
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-04-19 20:51:12 +0000

    Add: Now the user can draw

--- sketch.js
+++ sketch.js
@@ -17,3 +17,45 @@ function createGrid (rows, cols) {
 }
 
 createGrid(rows, cols);
+
+let mouseDown = false;
+
+document.body.addEventListener("mousedown", () => {
+  mouseDown = true;
+});
+
+document.body.addEventListener("mouseup", () => {
+  mouseDown = false;
+});
+
+const cells = document.querySelectorAll(".cols");
+
+cells.forEach(cell => {
+  // Change color when clicking directly
+  cell.addEventListener("mousedown", () => {
+    cell.style.backgroundColor = 'rgb(49,49,49)';
+  });
+
+  // Change color when hovering while mouse is down
+  cell.addEventListener("mouseover", () => {
+    if (mouseDown) {
+      cell.style.backgroundColor = 'rgb(49,49,49)';
+    }
+  });
+});
+
+const reset = document.querySelector(".reset");
+
+reset.addEventListener("click", () => {
+  cells.forEach(cell => {
+    cell.style.backgroundColor = 'rgb(218,218,218)';
+  });
+});
+
+// const numberInput = document.querySelector("number");
+
+// numberInput.addEventListener("change", () => {
+//   rows = numberInput.value;
+//   cols = rows*2;
+//   createGrid(rows, cols);
+// });
